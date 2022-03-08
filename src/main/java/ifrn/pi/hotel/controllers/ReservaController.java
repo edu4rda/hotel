d6a653cdf5b5b7b3ddcc490a689b9a26aacb68b7
Duda <damascenoe460@gmail.com>
2022-03-08 15:26:58 -0300

    Atualizando layout da aplicação

--- src/main/java/ifrn/pi/hotel/controllers/ReservaController.java
+++ src/main/java/ifrn/pi/hotel/controllers/ReservaController.java
@@ -1,9 +1,13 @@
 package ifrn.pi.hotel.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import ifrn.pi.hotel.models.Reserva;
 import ifrn.pi.hotel.repositories.ReservaRepository;
@@ -25,5 +29,13 @@ public class ReservaController {
 		System.out.println(reserva);
 		rr.save(reserva);
 		return "reserva-adicionada";
+	}
+	@GetMapping("/hotel")
+	public ModelAndView listar() {
+		
+		List<Reserva> reservas = rr.findAll();
+		ModelAndView mv = new ModelAndView("listaReserva");
+		mv.addObject("reservas", reservas);
+		return mv;
 	}
 }
