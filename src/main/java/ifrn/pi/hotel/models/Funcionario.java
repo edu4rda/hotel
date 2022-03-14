586914a3825d8ef9119a3000b1653f3a30240e3d
Duda <damascenoe460@gmail.com>
2022-03-14 16:37:17 -0300

    Adicionando classe do funcionário

--- src/main/java/ifrn/pi/hotel/models/Funcionario.java
+++ src/main/java/ifrn/pi/hotel/models/Funcionario.java
@@ -2,109 +2,77 @@ package ifrn.pi.hotel.models;
 
 import java.util.Collection;
 
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-@Entity
-public class Usuario implements UserDetails {
+
+public class Funcionario implements UserDetails {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String cpf;
-	private String email;
 	private String senha;
 	
-	@ManyToOne
-	private Reserva reserva; 
-	
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
 	public String getCpf() {
 		return cpf;
 	}
-
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
-
-	public String getEmail() {
-		return email;
+	public String getSenha() {
+		return senha;
 	}
-
-	public void setEmail(String email) {
-		this.email = email;
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
-	@Override
-	public String toString() {
-		return "Reserva [id=" + id + ", nome" + nome + ", CPF" + cpf + ", email" + email + "]";
-	}
-
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
 	@Override
 	public String getPassword() {
 		// TODO Auto-generated method stub
 		return this.senha;
 	}
-
 	@Override
 	public String getUsername() {
 		// TODO Auto-generated method stub
 		return this.cpf;
 	}
-
 	@Override
 	public boolean isAccountNonExpired() {
 		// TODO Auto-generated method stub
 		return true;
 	}
-
 	@Override
 	public boolean isAccountNonLocked() {
 		// TODO Auto-generated method stub
 		return true;
 	}
-
 	@Override
 	public boolean isCredentialsNonExpired() {
 		// TODO Auto-generated method stub
 		return true;
 	}
-
 	@Override
 	public boolean isEnabled() {
 		// TODO Auto-generated method stub
